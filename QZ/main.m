ca00eb24a754d75150ce5d68bdba20b37b68efae
songyang <aasyk2@126.com>
2016-06-14 16:03:35 +0800

    Initial Commit

---
+++ QZ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QZ
+//
+//  Created by 宋扬 on 16/6/14.
+//  Copyright © 2016年 handloft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
